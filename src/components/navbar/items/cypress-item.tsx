d94b224661b100d080517056005aa69f1321a782
cypressen <2050038998@qq.com>
2024-07-23 16:31:58 +0800

    add responsive navigation bar, deployed on vercel

--- src/components/navbar/items/cypress-item.tsx
+++ src/components/navbar/items/cypress-item.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
+import Image from "next/image";
 import { NavigationMenuItem } from "@/components/ui/navigation-menu";
 const CypressItem = () => {
   return (
     <NavigationMenuItem className="list-none">
       <Link href="/">
-        <img
+        <Image
           src="/cypress-logo.png"
           alt="cypress"
+          width={64}
+          height={28}
           className="flex h-7 w-16 ml-3"
         />
       </Link>
